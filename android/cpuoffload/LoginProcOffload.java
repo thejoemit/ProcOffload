bbe9568501be59fb15d0ae1a27f86e315037a3e3
thejoemit <the.joe.mit@gmail.com>
2016-06-09 02:01:08 -0400

    Update LoginProcOffload.java

--- android/cpuoffload/LoginProcOffload.java
+++ android/cpuoffload/LoginProcOffload.java
@@ -26,10 +26,33 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Random;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
+import static android.Manifest.permission.INTERNET;
 /**
  * A login screen that offers login via user/password.
  */
@@ -38,13 +61,11 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
+    private static final int REQUEST_READ_INTERNET = 0;
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
+     * A successful authentication will generate a SessID.
      */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "CARLETON1:helloworld", "CARLETON2:password"
-    };
+    private String SessID = null;
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -54,6 +75,11 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,6 +99,7 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
             }
         });
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        assert mEmailSignInButton != null;
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -81,7 +108,11 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
         });
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
             return;
@@ -97,7 +128,7 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
         }
         if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
             Snackbar.make(mUserView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
+                    .setAction(android.R.string.ok, new OnClickListener() {
                         @Override
                         @TargetApi(Build.VERSION_CODES.M)
                         public void onClick(View v) {
@@ -106,6 +137,27 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
                     });
         } else {
             requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+        }
+        return false;
+    }
+    private boolean mayRequestNetwork() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (checkSelfPermission(INTERNET) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        if (shouldShowRequestPermissionRationale(INTERNET)) {
+            Snackbar.make(mUserView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(android.R.string.ok, new OnClickListener() {
+                        @Override
+                        @TargetApi(Build.VERSION_CODES.M)
+                        public void onClick(View v) {
+                            requestPermissions(new String[]{INTERNET}, REQUEST_READ_INTERNET);
+                        }
+                    });
+        } else {
+            requestPermissions(new String[]{INTERNET}, REQUEST_READ_INTERNET);
         }
         return false;
     }
@@ -161,6 +213,7 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
+            mayRequestNetwork();
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
@@ -170,7 +223,6 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
         //TODO: Replace this with your own logic
         return email.contains("CARLETON");
     }
-
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
         return password.length() > 6;
@@ -216,12 +268,10 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
                 // Retrieve data rows for the device user's 'profile' contact.
                 Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
                         ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
                 // Select only email addresses.
                 ContactsContract.Contacts.Data.MIMETYPE +
                         " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                                                                     .CONTENT_ITEM_TYPE},
-
+                .CONTENT_ITEM_TYPE},
                 // Show primary email addresses first. Note that there won't be
                 // a primary email address if the user hasn't specified one.
                 ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
@@ -237,16 +287,69 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
     }
     @Override
     public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "LoginProcOffload Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://ninja.bitnet.cpuoffload/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "LoginProcOffload Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://ninja.bitnet.cpuoffload/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
     private interface ProfileQuery {
         String[] PROJECTION = {
                 ContactsContract.CommonDataKinds.Email.ADDRESS,
                 ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
         };
-
         int ADDRESS = 0;
         int IS_PRIMARY = 1;
+    }
+    /**
+     * Generates a Random integer
+     */
+    public int randomNumber(int min, int max) {
+        Random r = new Random();
+        return r.nextInt(max - min + 1) + min;
+    }
+    /**
+     * Generates a SHA512 Hash from a string
+     */
+    private static String getSHA512HashfromString(String str) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        md.update(str.getBytes());
+        byte byteData[] = md.digest();
+        //convert the byte to hex format method 1
+        StringBuffer hashCodeBuffer = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+            hashCodeBuffer.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return hashCodeBuffer.toString();
     }
     /**
      * Represents an asynchronous login/registration task used to authenticate
@@ -255,47 +358,96 @@ public class LoginProcOffload extends AppCompatActivity implements LoaderCallbac
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
         private final String mEmail;
         private final String mPassword;
-
         UserLoginTask(String email, String password) {
             mEmail = email;
             mPassword = password;
         }
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
+            String strreturn = null;
+            //Super secret sauce
+            int rnd = randomNumber(1000000000, 1999999999);
+            int rnd2 = randomNumber(1000000000, 1999999999);
+            String salt = String.valueOf(rnd).concat(String.valueOf(rnd2));
+            String hashPass = String.valueOf(rnd).concat(mPassword);
+            hashPass = hashPass.concat(String.valueOf(rnd2));
+            Snackbar.make(mUserView, hashPass, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
+                hashPass = getSHA512HashfromString(hashPass);
+                Snackbar.make(mUserView, hashPass, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
                 return false;
             }
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
+            try {
+                CertificateFactory cf = CertificateFactory.getInstance("X.509");
+                //enter your CRT file
+                InputStream caInput = new BufferedInputStream(getResources().openRawResource(R.raw.notoriousbit));
+                Certificate ca = cf.generateCertificate(caInput);
+                System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+                // Create a KeyStore containing our trusted CAs
+                String keyStoreType = KeyStore.getDefaultType();
+                KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+                keyStore.load(null, null);
+                keyStore.setCertificateEntry("ca", ca);
+                // Create a TrustManager that trusts the CAs in our KeyStore
+                String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+                TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+                tmf.init(keyStore);
+                //Now we can talk to the webserver using that certificate
+                String link = "https://www.login.php?user=" + mEmail + "&rid=" + salt + "&pass=" + hashPass;
+                URL url = new URL(link);
+                SSLContext context = SSLContext.getInstance("TLS");
+                context.init(null, tmf.getTrustManagers(), new java.security.SecureRandom());
+                HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                InputStreamReader iostr = new InputStreamReader(urlConnection.getInputStream());
+                BufferedReader in = new BufferedReader(iostr);
+                StringBuilder sb = new StringBuilder("");
+                String line;
+                while ((line = in.readLine()) != null) {
+                    sb.append(line);
+                    break;
+                }
+                in.close();
+                iostr.close();
+                strreturn = sb.toString();
+                } catch(IOException | NoSuchAlgorithmException | KeyManagementException | CertificateException | KeyStoreException e){
+                    e.printStackTrace();
+                }
+            assert strreturn != null;
+            String pieces[] = strreturn.split(":");
                 if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+                    SessID = pieces[1];
+                    Snackbar.make(mUserView, "USER: " + mEmail + " SESSID: " + SessID, Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    return true;
+                } else {
+                    Snackbar.make(mUserView, strreturn, Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    return false;
                 }
             }
-            // TODO: register the new account here.
-            return true;
-        }
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-            if (success) {
-                Intent intent = new Intent(getApplicationContext(), MainProcOffload.class);
-                startActivity(intent);
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
+            @Override
+            protected void onPostExecute ( final Boolean success){
+                mAuthTask = null;
+                showProgress(false);
+                if (success) {
+                    Intent intent = new Intent(getApplicationContext(), MainProcOffload.class);
+                    intent.putExtra("SessionID", SessID);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    mPasswordView.setError(getString(R.string.error_incorrect_password));
+                    mPasswordView.requestFocus();
+                }
+            }
+            @Override
+            protected void onCancelled () {
+                mAuthTask = null;
+                showProgress(false);
             }
         }
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
     }
-}
