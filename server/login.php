8ca00399f1290828596aa9607a664d3184e9e97c
thejoemit <the.joe.mit@gmail.com>
2016-06-09 01:56:30 -0400

    Create login.php

---
+++ server/login.php
@@ -0,0 +1,24 @@
+<?php
+$username = "CARLETON12";
+$password = "password";
+$sent_user = $_GET['user'];
+$sent_pass = $_GET['pass'];
+$sent_salt = $_GET['rid'];
+
+if (!empty($sent_user) and !empty($sent_pass) and !empty($sent_salt)) {
+if (strcmp($username,$sent_user) == '0') {
+        $salt1 = substr($sent_salt,0,10);
+        $salt2 = substr($sent_salt,10,10);
+        $hash_pass = hash('SHA512',$salt1.$password.$salt2);
+        if (strcmp($hash_pass,$sent_pass) == '0') {
+                echo "$username:1234567";
+        } else {
+                echo "password error";
+        }
+} else {
+ echo "username error";
+}
+} else {
+ echo "parameter error";
+}
+?>
